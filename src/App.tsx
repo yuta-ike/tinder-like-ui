44c88e4a95c68e1e8e610e1b94bc34ca181bd2bf
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-01-04 00:42:14 +0900

    feat: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import Card from "./components/Card"
+import Header from "./components/Header"
 import SwipeCardDeck from "./components/SwipeCardDeck"
 
 const App: React.VFC = () => {
   return (
     <>
-      <footer className="fixed inset-x-0 top-0 w-screen p-2">
-        <div className="flex w-full h-full text-white bg-[#525b66] rounded shadow items-center justify-between p-2">
-          <h1 className="mx-1">Tinder like UI Sample</h1>
-          <a
-            href="https://github.com/yuta-ike/tinder-like-ui"
-            className="px-2 py-1 text-sm border border-white rounded"
-            target="_blank"
-            rel="external nofollow noopener noreferrer"
-            aria-label="Githubで見る"
-          >
-            Github
-          </a>
-        </div>
-      </footer>
+      <Header />
       <div className="flex items-center justify-center w-screen h-screen overflow-hidden">
         <SwipeCardDeck
-          count={10}
+          count={6}
           render={(index, selected) => (
             <div>
               <Card title={`レンジで簡単！ジューシーハンバーグ:${index}`} showShadow={selected} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Header: React.VFC = () => {
+  return (
+    <header className="fixed inset-x-0 top-0 w-screen p-2">
+      <div className="flex w-full h-full text-white bg-[#525b66] rounded shadow items-center justify-between p-2">
+        <h1 className="mx-1">Tinder like UI Sample</h1>
+        <a
+          href="https://github.com/yuta-ike/tinder-like-ui"
+          className="px-2 py-1 text-sm border border-white rounded outline-none hover:bg-white/10 focus:bg-white/10 focus:ring focus:ring-slate-400"
+          target="_blank"
+          rel="external nofollow noopener noreferrer"
+          aria-label="Githubで見る"
+        >
+          Github
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
